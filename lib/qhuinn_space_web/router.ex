5a2a449d275b8df20599363e27280b82c2f60eb3
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2018-02-11 06:19:19 -0800

    Use textarea

--- lib/qhuinn_space_web/router.ex
+++ lib/qhuinn_space_web/router.ex
@@ -17,6 +17,9 @@ defmodule QhuinnSpaceWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/stories", StoryController
+
+
   end
 
   # Other scopes may use custom stacks.
